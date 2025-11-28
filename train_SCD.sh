643f10e037227e97a70c2ec260d14182f168faa9
Matteo Taiana <matteo.taiana@lab0.com>
2025-11-28 14:17:36 +0100

    There are now two setups for SCD and 202408.

--- train_SCD.sh
+++ train_SCD.sh
@@ -1,6 +1,6 @@
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 python3 main.py fit \
-  -c configs/dinov3/coco/instance/eomt_large_640.yaml \
+  -c configs/dinov3/coco/instance/eomt_large_640_SCD.yaml \
   --trainer.devices 1 \
   --trainer.accumulate_grad_batches 16 \
   --data.batch_size 1 \
--- validate_SCD.sh
+++ validate_SCD.sh
@@ -1,6 +1,6 @@
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 python3 main.py validate \
-   -c configs/dinov3/coco/instance/eomt_large_640.yaml \
+   -c configs/dinov3/coco/instance/eomt_large_640_SCD.yaml \
    --ckpt_path '/workspace/eomt/checkpoints/Run2/epoch=055-mAP=0.81.ckpt' \
    --data.path /workspace/data/Datasets/SCD_low_res_clean/ \
    --data.batch_size 1
